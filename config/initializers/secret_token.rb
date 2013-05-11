cbefeb8e9e1c564bd8aff2f1bcda5070f283ac4f
Oscar Rieken <oriekenjr@gmail.com>
2013-05-10 21:32:48 -0400

    baseline implementation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhatsTheGameGuys::Application.config.secret_key_base = '4dc3284fe2496c86594b342020175db41684ed62c9534c3bef871980dc076004d2c914841fa5adf202eae0a10e1a64bdaaaf1c800d3dd69ccef6238d4c35f7e7'
